a7dafe2392486bf0efc39176f8b639b0c700c378
vasanthonline <vasanthgonline@gmail.com>
2020-11-22 16:57:26 +0530

    Add tests for 001-http application with mockmvc

--- 001-http/src/test/kotlin/com/example/http/HttpApplicationTests.kt
+++ 001-http/src/test/kotlin/com/example/http/HttpApplicationTests.kt
@@ -1,13 +1,27 @@
 package com.example.http
 
+import org.hamcrest.Matchers.containsString
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
 
 @SpringBootTest
-class HttpApplicationTests {
+@AutoConfigureMockMvc
+class HttpApplicationTests @Autowired constructor(
+		private val mockMvc: MockMvc
+) {
 
 	@Test
-	fun contextLoads() {
+	fun httpEndpointTest() {
+		mockMvc.perform(get("/")).andDo(print()).andExpect(status().is2xxSuccessful)
+				.andExpect(content().string(containsString("Hello world!")));
 	}
 
 }
